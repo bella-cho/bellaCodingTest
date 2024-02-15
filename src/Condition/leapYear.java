a25fc36c05ac89a987580f110154c3d5ebd14836
hyun-joung <hyunjoung.dev@gmail.com>
2024-02-15 16:53:11 +0900

    Initial commit

---
+++ src/Condition/leapYear.java
@@ -0,0 +1,31 @@
+package Condition;
+
+import java.util.Scanner;
+
+public class leapYear {
+    public static void main(String[] args){
+
+
+        Scanner sc= new Scanner(System.in);
+        int a= sc.nextInt();
+
+
+        if(a%4==0){
+            if (a%400==0){
+                System.out.println("1");
+            } else if (a%100==0) {
+                System.out.println("0");
+            } else {
+                System.out.println("0");
+                //if .. else if.. else 로 마무리해야함
+            }
+        } else {
+            System.out.println("0");
+        }
+
+
+
+
+    }
+}
+
